6a2513fcd1227cd9339123b251a7fca353816618
Nora Xiao <yixuan.xiao@hotmail.com>
2020-03-21 16:23:42 -0700

    Add files via upload

---
+++ Array/MinimumSwaps2.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class MinimumSwaps2 {
+    // Complete the minimumSwaps function below.
+    static int minimumSwaps(int[] arr) {
+        int swaps = 0;
+        for (int i = 0; i < arr.length; i++) {
+            // for example, 3
+            int currentValue = arr[i];
+            // for example, 2
+            int currentLocation = i + 1;
+            while (currentValue != currentLocation) {
+                // arr[1] = arr[3 - 1];
+                arr[i] = arr[currentValue - 1];
+                arr[currentValue - 1] = currentValue;
+                swaps++;
+
+                currentValue = arr[i];
+            }
+        }
+        return swaps;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int res = minimumSwaps(arr);
+
+        bufferedWriter.write(String.valueOf(res));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
