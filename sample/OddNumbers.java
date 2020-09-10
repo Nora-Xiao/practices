c02c7ba7e8ba06b970a06b042fc690cab6ff2a67
Nora Xiao <yixuan.xiao@hotmail.com>
2020-09-10 08:26:54 -0700

    Add files via upload

---
+++ sample/OddNumbers.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+class Result {
+
+    /*
+     * Complete the 'oddNumbers' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER l
+     *  2. INTEGER r
+     */
+
+    public static List<Integer> oddNumbers(int l, int r) {
+        // Write your code here
+        // 除以2无余数，是even number
+        int startNum;
+        if (l % 2 == 0) {
+            startNum = l + 1;
+        } else {
+            startNum = l;
+        }
+
+        List<Integer> ret = new ArrayList<>();
+        for (int i = startNum; i <= r; i += 2) {
+            ret.add(i);
+        }
+        return ret;
+    }
+
+}
+public class OddNumbers {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int l = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int r = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> result = Result.oddNumbers(l, r);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining("\n"))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
